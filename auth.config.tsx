de7b1573a9db3063c9963391f841b0cee0efab7b
shahorozco <shah.orozco@qcode.co>
2024-03-09 21:01:11 -0500

    deploy_master: se añaden todas las paginas a callback

--- auth.config.tsx
+++ auth.config.tsx
@@ -8,7 +8,9 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/ui/dashboard');
-      if (isOnDashboard) {
+      const isOnInvoices = nextUrl.pathname.startsWith('/ui/invoices');
+      const isOnCustomer = nextUrl.pathname.startsWith('/ui/customers');
+      if (isOnDashboard || isOnInvoices || isOnCustomer) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
