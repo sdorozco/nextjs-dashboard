100c6685860202a7cc9ae9f83af528d272dc1efb
shahorozco <shah.orozco@qcode.co>
2024-08-29 23:08:04 -0500

    feat: se organiza layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
         {children}
-        <SpeedInsights />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
